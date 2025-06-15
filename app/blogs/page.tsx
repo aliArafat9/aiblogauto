7f07e2537bd4885a42d0829b0adb3fb6d138a056
Arafat <arfatakbar090@gmail.com>
2025-06-15 18:48:34 +0530

    updates

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -5,23 +5,31 @@ import Link from "next/link";
 import BlogCard from "@/components/blog/blog-card";
 import BlogPagination from "@/components/blog/blog-pagination";
 
-export default async function BlogsPage({
-  searchParams,
-}: {
-  searchParams?: { page?: string };
-}) {
-  const page = searchParams?.page ? parseInt(searchParams.page, 10) : 1;
-  const limit = 3;
 
-  const { blogs, totalCount } = await getAllBlogsDb(page, limit);
+export const metadata = {
+  title: "Blogs",
+  description: "Find the latest blogs",
+};
+
+interface BlogsPageProps {
+  searchParams: Promise<{ page?: number }>;
+}
+
+export default async function BlogsPage({ searchParams }: BlogsPageProps) {
+  const { page } = await searchParams; // Await and destructure the `page` from the resolved promise
+  const currentPage = page ? parseInt(page as unknown as string, 10) : 1;
+  const limit = 12; // Set the number of blogs per page
+
+  const { blogs, totalCount } = await getAllBlogsDb(currentPage, limit); // Make sure your action supports pagination
   const totalPages = Math.ceil(totalCount / limit);
 
   return (
     <div className="md:mt-0">
       <div className="p-5">
-        <h1 className="text-2xl font-bold">Explore the latest blog posts</h1>
-
-        <p className="text-sm text-gray-500">Total blog posts: {totalCount}</p>
+        <h1 className="text-2xl font-bold">Explore the Latest Blog Posts</h1>
+        <p className="text-sm text-gray-500">
+          Total blog posts: {totalPages * limit}
+        </p>
         <br />
 
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
@@ -36,11 +44,10 @@ export default async function BlogsPage({
             </div>
           ))}
         </div>
+      </div>
 
-        {/* pagination */}
-        <div className="flex justify-center mt-5">
-          <BlogPagination page={page} totalPages={totalPages} />
-        </div>
+      <div className="flex justify-center mt-5">
+        <BlogPagination page={currentPage} totalPages={totalPages} />
       </div>
     </div>
   );
